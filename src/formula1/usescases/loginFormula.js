23b83a21aa0273c56d495436d03f43d907c80f40
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-01-30 08:53:40 -0500

    feat: :sparkles: Add loginFormula Login for administator

--- src/formula1/usescases/loginFormula.js
+++ src/formula1/usescases/loginFormula.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 export class loginFormula extends HTMLElement{
     constructor(){
         super();
@@ -6,9 +8,40 @@ export class loginFormula extends HTMLElement{
     render(){
         //Login Admin
         this.innerHTML= /*html*/ `
-        
-        `
+        <form>
+            <div class="form-group">
+                <label for="userAdmin">Administrador</label>
+                <input type="text" class="form-control" id="userAdmin" placeholder="Ingrese su usuario">
+            </div>
+            <div class="form-group">
+                <label for="passwordAdmin">Contraseña</label>
+                <input type="password" class="form-control" id="passwordAdmin" placeholder="Ingrese su contraseña">
+            </div>
+            <button id="btnIngresar" type="submit" class="btn btn-primary">Ingresar</button>
+            </form>
+        `;
+        this.addEventListener();
+    }
 
+    addEventListener(){
+        document.querySelector('#btnIngresar').addEventListener('click', () => {
+            this.ingresoAdmin(); 
+        });
+    }
+
+    ingresoAdmin(){
+        let usuario = this.querySelector("#userAdmin").value;
+        let contraseña = this.querySelector("#passwordAdmin").value;
+        if(usuario === "admin" && contraseña === "admin123"){
+            //window.location.hash = "#/admin";
+            alert("Bienvenido Admin");
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Usuario o contraseña incorrecto!",
+            });
+        }
     }
 }
 
