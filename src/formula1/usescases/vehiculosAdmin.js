f3604dddab2012271cd5345f16c3d152f0c1d67a
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-02-01 19:06:17 -0500

    feat: :sparkles: Crear Vehiculos

--- src/formula1/usescases/vehiculosAdmin.js
+++ src/formula1/usescases/vehiculosAdmin.js
@@ -7,7 +7,7 @@ export class VehiculosAdmin extends HTMLElement {
         this.currentStep = 1;
         this.render();
         this.setupEventListeners();
-        this.crearVehiculos();
+        this.addEventListeners();
     }
 
     setupEventListeners() {
@@ -54,6 +54,7 @@ export class VehiculosAdmin extends HTMLElement {
     }
 
     render() {
+        let idVehiculo = Date.now();
         this.shadowRoot.innerHTML = /*html*/ `
         <style>
             :host {
@@ -281,16 +282,9 @@ export class VehiculosAdmin extends HTMLElement {
             <div class="step-section" data-step="1">
                 <h3>Información Básica</h3>
                 <div class="form-group">
-                        <label class="form-label" for="imagenVehiculo">Imagen Vehiculo</label>
-                        <div class="image-upload-container" id="dropZone">
-                            <i>📁</i>
-                            <p class="image-upload-text">Arrastra una imagen aquí o haz clic para seleccionar</p>
-                            <input type="file" class="file-input" id="imagenVehiculo" name="imagenVehiculo" accept="image/*">
+                        <label for="imagenVehiculo">Imagen (URL)</label>
+                        <input type="url" id="imagenVehiculo" name="imagenVehiculo" placeholder="URL de la imagen">
                         </div>
-                        <div class="preview-container">
-                            <img class="preview-image" id="imagePreview" alt="Preview">
-                        </div>
-                </div>
                 <div class="mb-3">
                     <label for="motor" class="form-label">Motor</label>
                     <input type="text" class="form-control" name="motor" id="motor" aria-describedby="NombreHelp">
@@ -321,11 +315,11 @@ export class VehiculosAdmin extends HTMLElement {
                 </div>
                 <div class="mb-3">
                     <label for="velocidad_maxima_kmh" class="form-label">Velocidad Máxima</label>
-                    <input type="text" class="form-control" id="velocidad_maxima_kmh" name="velocidad_maxima_kmh" aria-describedby="ModeloHelp">
+                    <input type="number" class="form-control" id="velocidad_maxima_kmh" name="velocidad_maxima_kmh" aria-describedby="ModeloHelp">
                 </div>
                 <div class="mb-3">
                     <label for="aceleracion_0_100" class="form-label">Aceleración</label>
-                    <input type="text" class="form-control" id="aceleracion_0_100" name="aceleracion_0_100" aria-describedby="ModeloHelp">
+                    <input type="number" class="form-control" id="aceleracion_0_100" name="aceleracion_0_100" aria-describedby="ModeloHelp">
                 </div>
                 <div class="step-navigation">
                     <button type="button" class="step-btn prev-step">Anterior</button>
@@ -337,21 +331,21 @@ export class VehiculosAdmin extends HTMLElement {
             <div class="step-section" data-step="3">
                 <h3>Conducción Normal</h3>
                 <label for="velocidad_normal" class="form-label">Velocidad Promedio</label>
-                <input type="text" class="form-control" id="velocidad_normal" name="velocidad_normal" aria-describedby="VelocPromHelp">
+                <input type="number" class="form-control" id="velocidad_normal" name="velocidad_normal" aria-describedby="VelocPromHelp">
                 <p>Consumo de Combustible</p>
                 <label for="seco_normal" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_normal" name="seco_normal" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_normal" name="seco_normal" aria-describedby="SecoHelp">
                 <label for="lluvioso_normal" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_normal" id="lluvioso_normal" aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_normal" id="lluvioso_normal" aria-describedby="LluviosoHelp">
                 <label for="extremo_normal" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_normal" name="extremo_normal" aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_normal" name="extremo_normal" aria-describedby="ExtremoHelp">
                 <p>Desgaste de Neumaticos</p>
                 <label for="seco_neumaticos" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_neumaticos" name="seco_neumaticos" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_neumaticos" name="seco_neumaticos" aria-describedby="SecoHelp">
                 <label for="lluvioso_neumaticos" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_neumaticos" name="lluvioso_neumaticos" aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_neumaticos" name="lluvioso_neumaticos" aria-describedby="LluviosoHelp">
                 <label for="extremo_neumaticos" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_neumaticos" name="extremo_neumaticos" aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_neumaticos" name="extremo_neumaticos" aria-describedby="ExtremoHelp">
                 <div class="step-navigation">
                     <button type="button" class="step-btn prev-step">Anterior</button>
                     <button type="button" class="step-btn next-step">Siguiente: Conducción Agresiva</button>
@@ -362,21 +356,21 @@ export class VehiculosAdmin extends HTMLElement {
             <div class="step-section" data-step="4">
                 <h3>Conducción Agresiva</h3>
                 <label for="velocidad_agresiva" class="form-label">Velocidad Promedio</label>
-                <input type="text" class="form-control" id="velocidad_agresiva" name="velocidad_agresiva" aria-describedby="VelocPromHelp">
+                <input type="number" class="form-control" id="velocidad_agresiva" name="velocidad_agresiva" aria-describedby="VelocPromHelp">
                 <p>Consumo de Combustible</p>
                 <label for="seco_agresiva" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_agresiva" name="seco_agresiva" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_agresiva" name="seco_agresiva" aria-describedby="SecoHelp">
                 <label for="lluvioso_agresiva" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_agresiva" name="lluvioso_agresiva" aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_agresiva" name="lluvioso_agresiva" aria-describedby="LluviosoHelp">
                 <label for="extremo_agresiva" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_agresiva" aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_agresiva" aria-describedby="ExtremoHelp">
                 <p>Desgaste de Neumaticos</p>
                 <label for="seco_agreneu" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_agreneu" name="seco_agreneu" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_agreneu" name="seco_agreneu" aria-describedby="SecoHelp">
                 <label for="lluvioso_agreneu" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_agreneu" name="lluvioso_agreneu"  aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_agreneu" name="lluvioso_agreneu"  aria-describedby="LluviosoHelp">
                 <label for="extremo_agreneu" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_agreneu" name="extremo_agreneu"  aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_agreneu" name="extremo_agreneu"  aria-describedby="ExtremoHelp">
                 <div class="step-navigation">
                     <button type="button" class="step-btn prev-step">Anterior</button>
                     <button type="button" class="step-btn next-step">Siguiente: Ahorro de Combustible</button>
@@ -387,24 +381,24 @@ export class VehiculosAdmin extends HTMLElement {
             <div class="step-section" data-step="5">
                 <h3>Ahorro de Combustible</h3>
                 <label for="velocidad_combustible" class="form-label">Velocidad Promedio</label>
-                <input type="text" class="form-control" id="velocidad_combustible" name="velocidad_combustible"  aria-describedby="VelocPromHelp">
+                <input type="number" class="form-control" id="velocidad_combustible" name="velocidad_combustible"  aria-describedby="VelocPromHelp">
                 <p>Consumo de Combustible</p>
                 <label for="seco_ahorro" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_ahorro" name="seco_ahorro" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_ahorro" name="seco_ahorro" aria-describedby="SecoHelp">
                 <label for="lluvioso_ahorro" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_ahorro" name="lluvioso_ahorro" aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_ahorro" name="lluvioso_ahorro" aria-describedby="LluviosoHelp">
                 <label for="extremo_ahorro" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_ahorro" name="extremo_ahorro"  aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_ahorro" name="extremo_ahorro"  aria-describedby="ExtremoHelp">
                 <p>Desgaste de Neumaticos</p>
                 <label for="seco_ahorro_neu" class="form-label">Seco</label>
-                <input type="text" class="form-control" id="seco_ahorro_neu" name="seco_ahorro_neu" aria-describedby="SecoHelp">
+                <input type="number" class="form-control" id="seco_ahorro_neu" name="seco_ahorro_neu" aria-describedby="SecoHelp">
                 <label for="lluvioso_ahorro_neu" class="form-label">Lluvioso</label>
-                <input type="text" class="form-control" id="lluvioso_ahorro_neu" name="lluvioso_ahorro_neu" aria-describedby="LluviosoHelp">
+                <input type="number" class="form-control" id="lluvioso_ahorro_neu" name="lluvioso_ahorro_neu" aria-describedby="LluviosoHelp">
                 <label for="extremo_ahorro_neu" class="form-label">Extremo</label>
-                <input type="text" class="form-control" id="extremo_ahorro_neu" name="extremo_ahorro_neu" aria-describedby="ExtremoHelp">
+                <input type="number" class="form-control" id="extremo_ahorro_neu" name="extremo_ahorro_neu" aria-describedby="ExtremoHelp">
                 <div class="step-navigation">
                     <button type="button" class="step-btn prev-step">Anterior</button>
-                    <button type="submit" btn="btnRegistrarVehiculo" class="btn-submit">Guardar Vehículo</button>
+                    <button type="button" id="btnRegistrarVehiculo" class="btn-submit">Guardar Vehículo</button>
                 </div>
             </div>
             <div id="statusMessage" class="status-message"></div>
@@ -451,96 +445,84 @@ export class VehiculosAdmin extends HTMLElement {
         });
     }
 
-    crearVehiculos() {
-        const formCrearVehiculo = this.shadowRoot.querySelector('#formCrearVehiculo');
-        const dropZone = this.shadowRoot.querySelector('#dropZone');
-        const fileInput = this.shadowRoot.querySelector('.file-input');
-        const previewContainer = this.shadowRoot.querySelector('.preview-container');
-        const imagePreview = this.shadowRoot.querySelector('#imagePreview');
-        const statusMessage = this.shadowRoot.querySelector('#statusMessage');
+    addEventListeners() {
+        this.shadowRoot.querySelector('#btnRegistrarVehiculo').addEventListener("click", () => this.crearVehiculos());
+    }    
 
-        const handleImageDisplay = (file) => {
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = (e) => {
-                    imagePreview.src = e.target.result;
-                    previewContainer.style.display = 'block';
-                };
-                reader.readAsDataURL(file);
+    crearVehiculos = () => {
+        const formCrearVehiculo = this.shadowRoot.querySelector('#formCrearVehiculo');
+        const statusMessage = this.shadowRoot.querySelector('#statusMessage');
+    
+        const formData = new FormData(formCrearVehiculo);
+        const datos = Object.fromEntries(formData.entries());
+    
+        const vehiculo = {
+            imagenVehiculo: datos.imagenVehiculo,
+            motor: datos.motor,
+            modelo: datos.modelo,
+            equipoPiloto: datos.equipoPiloto,
+            nombrePiloto: datos.nombrePiloto,
+            velocidadMaximaKmh: datos.velocidad_maxima_kmh,
+            aceleracion0a100: datos.aceleracion_0_100,
+            conduccionNormal: {
+                velocidadNormal: datos.velocidad_normal,
+                consumoCombustible: {
+                    seco: datos.seco_normal,
+                    lluvioso: datos.lluvioso_normal,
+                    extremo: datos.extremo_normal
+                },
+                desgasteNeumaticos: {
+                    seco: datos.seco_neumaticos,
+                    lluvioso: datos.lluvioso_neumaticos,
+                    extremo: datos.extremo_neumaticos
+                }
+            },
+            conduccionAgresiva: {
+                velocidadAgresiva: datos.velocidad_agresiva,
+                consumoCombustible: {
+                    seco: datos.seco_agresiva,
+                    lluvioso: datos.lluvioso_agresiva,
+                    extremo: datos.extremo_agresiva
+                },
+                desgasteNeumaticos: {
+                    seco: datos.seco_agreneu,
+                    lluvioso: datos.lluvioso_agreneu,
+                    extremo: datos.extremo_agreneu
+                }
+            },
+            ahorroCombustible: {
+                velocidadPromedio: datos.velocidad_combustible,
+                consumoCombustible: {
+                    seco: datos.seco_ahorro,
+                    lluvioso: datos.lluvioso_ahorro,
+                    extremo: datos.extremo_ahorro
+                },
+                desgasteNeumaticos: {
+                    seco: datos.seco_ahorro_neu,
+                    lluvioso: datos.lluvioso_ahorro_neu,
+                    extremo: datos.extremo_ahorro_neu
+                }
             }
         };
 
-        dropZone.addEventListener('click', () => fileInput.click());
-        
-        dropZone.addEventListener('dragover', (e) => {
-            e.preventDefault();
-            dropZone.style.borderColor = 'var(--accent-color)';
-            dropZone.style.backgroundColor = 'rgba(99, 102, 241, 0.1)';
-        });
-
-        dropZone.addEventListener('dragleave', (e) => {
-            e.preventDefault();
-            dropZone.style.borderColor = 'var(--border-color)';
-            dropZone.style.backgroundColor = 'transparent';
-        });
-
-        dropZone.addEventListener('drop', (e) => {
-            e.preventDefault();
-            const file = e.dataTransfer.files[0];
-            if (file && file.type.startsWith('image/')) {
-                fileInput.files = e.dataTransfer.files;
-                handleImageDisplay(file);
-            }
-            dropZone.style.borderColor = 'var(--border-color)';
-            dropZone.style.backgroundColor = 'transparent';
-        });
-
-        fileInput.addEventListener('change', (e) => {
-            const file = e.target.files[0];
-            if (file) {
-                handleImageDisplay(file);
-            }
-        });// Manejo del envío del formulario
-        this.shadowRoot.querySelector('#btnRegistrarVehiculo').addEventListener("click", (e) => {
-            e.preventDefault();
-            const formData = new FormData(formCrearVehiculo);
-
-            const file = fileInput.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    const datos = Object.fromEntries(formData.entries());
-                    datos.imagenVehiculo = reader.result;
-
-                    postVehiculos(datos)
-                        .then(response => {
-                            if (response.ok) {
-                                return response.json();
-                            }
-                            throw new Error(`Error: ${response.status} - ${response.statusText}`);
-                        })
-                        .then(responseData => {
-                            console.log('Respuesta exitosa:', responseData);
-                            statusMessage.textContent = '¡Vehiculo registrado exitosamente!';
-                            statusMessage.className = 'status-message success';
-                            statusMessage.style.display = 'block';
-                            formCrearVehiculo.reset();
-                            previewContainer.style.display = 'none';
-                            setTimeout(() => {
-                                statusMessage.style.display = 'none';
-                            }, 3000);
-                        })
-                        .catch(error => {
-                            console.error('Error:', error.message);
-                            statusMessage.textContent = 'Error al registrar el vehiculo. Por favor, intente nuevamente.';
-                            statusMessage.className = 'status-message error';
-                            statusMessage.style.display = 'block';
-                        });
-                };
-                reader.readAsDataURL(file);
-            }
-        });
-    }
+        postVehiculos(vehiculo)
+            .then(response => response.json())
+            .then(responseData => {
+                statusMessage.textContent = '¡Vehículo registrado exitosamente!';
+                statusMessage.className = 'status-message success';
+                statusMessage.style.display = 'block';
+                formCrearVehiculo.reset();
+                setTimeout(() => {
+                    statusMessage.style.display = 'none';
+                }, 3000);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                statusMessage.textContent = 'Error al registrar el vehículo.';
+                statusMessage.className = 'status-message error';
+            });
+    };
+    
 }
 
 customElements.define("vehiculos-admin", VehiculosAdmin);
